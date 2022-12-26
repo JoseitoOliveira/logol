369e38070df16c423e3ff13604605a01f518b5b9
Joseíto <joseito.junior@brphotonics.com>
2022-12-26 09:41:37 -0300

    changes author name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='logol',
     version=json.load(open('pkg_info.json'))['__version__'],
-    author=['Joseíto Júnior'],
+    author=['Joseíto Oliveira'],
     python_requires='>3.8',
     url='https://github.com/JoseitoOliveira/logol',
     license='MIT',
