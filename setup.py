aa6699189373955de76da758a0e1a042f9e5adc3
Joseíto <joseito.junior@brphotonics.com>
2022-12-26 10:35:39 -0300

    removes pkg_info.json

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-import json
-
 from setuptools import setup, find_packages
 
 setup(
     name='logol',
     packages=['logol'],
-    version=json.load(open('pkg_info.json'))['__version__'],
+    version='0.0.2',
     author=['Joseíto Oliveira'],
     python_requires='>3.8',
     url='https://github.com/JoseitoOliveira/logol',
