8be66fa273b588422390d26086bc3ce14c5b1cde
Joseíto <joseito.junior@brphotonics.com>
2022-12-26 09:40:57 -0300

    add url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     version=json.load(open('pkg_info.json'))['__version__'],
     author=['Joseíto Júnior'],
     python_requires='>3.8',
+    url='https://github.com/JoseitoOliveira/logol',
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3.8',
